fc54f6599099f2ef42e9ae4802d4a0eeecc611c1
Evgeniy <95440463+MEvgeniy21@users.noreply.github.com>
2022-05-20 21:46:19 +0300

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,20 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientListEl = document.querySelector("[id='ingredients']");
+
+const ingredientItemEl = ingredients.reduce((acc, item) => {
+  const element = document.createElement("li");
+  element.textContent = item;
+  element.classList.add("item");
+  acc.push(element);
+  return acc;
+}, []);
+
+ingredientListEl.append(...ingredientItemEl);
