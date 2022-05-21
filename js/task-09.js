2a56dd5f6767699d430406ba0e60617c30b47fe5
Evgeniy <95440463+MEvgeniy21@users.noreply.github.com>
2022-05-21 21:41:47 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,20 @@
+const refs = {
+  body: document.body,
+  colorValue: document.querySelector(".widget .color"),
+  btnChangeColor: document.querySelector(".widget .change-color"),
+};
+
+refs.btnChangeColor.addEventListener("click", changeColorBG);
+
+function changeColorBG() {
+  changeColorValue();
+  refs.body.style.backgroundColor = refs.colorValue.textContent;
+}
+
+function changeColorValue() {
+  refs.colorValue.textContent = getRandomHexColor();
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
