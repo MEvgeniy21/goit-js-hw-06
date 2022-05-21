8c5217a07f069e7bf6cc67601af36a96b95d4399
Evgeniy <95440463+MEvgeniy21@users.noreply.github.com>
2022-05-21 21:23:11 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  const formRefs = event.currentTarget;
+  const elements = formRefs.elements;
+  const alertMessage = "Нужно заполнить все поля формы!";
+  const formData = {};
+
+  event.preventDefault();
+
+  if (!elements.email.value.length || !elements.password.value.length) {
+    alert(alertMessage);
+  } else {
+    formData[elements[0].name] = elements[0].value;
+    formData[elements[1].name] = elements[1].value;
+    console.log(formData);
+    formRefs.reset();
+  }
+}
